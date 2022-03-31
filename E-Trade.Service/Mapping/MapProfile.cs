b15205729859fa3f03f07630700ed44a2d00839c
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-03-31 16:45:36 +0300

    AutoMapper package added, MapProfile class added

---
+++ E-Trade.Service/Mapping/MapProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using E_Trade.Core.DTOs;
+using E_Trade.Core.Models;
+
+namespace E_Trade.Service.Mapping
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+        }
+    }
+}

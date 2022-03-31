9b2307d2b3d5822261446203c67a2b155559bef7
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-03-31 15:04:27 +0300

    Core layer updated

---
+++ E-Trade.Core/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace E_Trade.Core.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ E-Trade.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Trade.Core.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> GetByIdAsync(int id);
+        IQueryable<TEntity> GetAll();
+        IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<TEntity, bool>> expression);
+        Task AddAsync(TEntity entity);
+        void Update(TEntity entity);
+        void Remove(TEntity entity);
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+        void RemoveRange(IEnumerable<TEntity> entities);
+
+    }
+}

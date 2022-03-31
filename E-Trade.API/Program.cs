22fdc0630e0ccecde2e3f9636fe1e8224863a616
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-03-31 16:24:00 +0300

    database configuration and migration folder added

--- E-Trade.API/Program.cs
+++ E-Trade.API/Program.cs
@@ -1,3 +1,11 @@
+using E_Trade.Core.Repositories;
+using E_Trade.Core.UnitOfWorks;
+using E_Trade.Repository;
+using E_Trade.Repository.Repositories;
+using E_Trade.Repository.UnitOfWorks;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +14,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
+
+builder.Services.AddDbContext<ETradeDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PSqlConnection"), option =>
+    {
+        option.MigrationsAssembly(Assembly.GetAssembly(typeof(ETradeDbContext)).GetName().Name);
+    });
+});
 
 var app = builder.Build();
 

c469feb5428dfab69fc25c6a356c46737c0f0e3c
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-04-01 14:42:57 +0300

    ProductValidator added

---
+++ E-Trade.Service/Validations/ProductDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using E_Trade.Core.DTOs;
+using FluentValidation;
+
+namespace E_Trade.Service.Validations
+{
+    public class ProductDtoValidator : AbstractValidator<ProductDto>
+    {
+        public ProductDtoValidator()
+        {
+            RuleFor(x => x.Name).NotNull().WithMessage("{PropertyName} alanı null olamaz").NotEmpty().WithMessage("{PropertyName} alanı boş olamaz");
+            RuleFor(x => x.Price).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} alanı minimum 1 olamlıdır");
+            RuleFor(x => x.Description).NotNull().WithMessage("{PropertyName} alanı null olamaz").NotEmpty().WithMessage("{PropertyName} alanı boş olamaz");
+            RuleFor(x => x.ImageUrl).NotNull().WithMessage("{PropertyName} alanı null olamaz").NotEmpty().WithMessage("{PropertyName} alanı boş olamaz");
+            RuleFor(x => x.Quantity).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} alanı minimum 1 olmalıdır");
+            RuleFor(x => x.CategoryId).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} alanı minimum 1 olmalıdır");
+        }
+    }
+}

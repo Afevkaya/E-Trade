a22b9a16a8f46e07bfc93ff5dc611fd4f13c9ab9
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-04-01 14:46:12 +0300

    CatgeoryDtoValidator added

---
+++ E-Trade.Service/Validations/CategoryDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using E_Trade.Core.DTOs;
+using FluentValidation;
+
+namespace E_Trade.Service.Validations
+{
+    public class CategoryDtoValidator : AbstractValidator<CategoryDto>
+    {
+        public CategoryDtoValidator()
+        {
+            RuleFor(x => x.Name).NotNull().WithMessage("{PropertyName} alanı null olamaz").NotEmpty().WithMessage("{PropertyName} alanı boş olamaz");
+        }
+    }
+}

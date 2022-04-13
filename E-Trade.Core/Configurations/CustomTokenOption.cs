831d9f3747ddb7dd9f426aced65413287aef6432
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-04-13 11:25:05 +0300

    Core Layer: adding CustomTokenOption classes and adding TokenOption appsetting.json files.

---
+++ E-Trade.Core/Configurations/CustomTokenOption.cs
@@ -0,0 +1,12 @@
+﻿namespace E_Trade.Core.Configurations
+{
+    public class CustomTokenOption
+    {
+        public List<string> Audience { get; set; }
+        public string Issuer { get; set; }
+        public DateTime AccessTokenExpiration { get; set; }
+        public DateTime RefreshTokenExpiration { get; set; }
+        public string SecurityKey { get; set; }
+
+    }
+}

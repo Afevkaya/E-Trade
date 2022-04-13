b30194cbeb1aea9176fac672aaa32e8ade63f9eb
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-04-13 11:14:55 +0300

    Service Layer: Adding SignService and updated AspNetCore.Identity package.

---
+++ E-Trade.Service/Services/SignService.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace E_Trade.Service.Services
+{
+    public static class SignService
+    {
+        public static SecurityKey GetSymmetricSecurityKey(string securityKey)
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+        }
+
+    }
+}

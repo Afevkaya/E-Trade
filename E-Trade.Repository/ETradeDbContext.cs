ef7979283a36ee07dd5c4dc60b4cd32c64c5946f
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-04-13 10:50:34 +0300

    Core Layer: Changing the identity package.

--- E-Trade.Repository/ETradeDbContext.cs
+++ E-Trade.Repository/ETradeDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using E_Trade.Core.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 namespace E_Trade.Repository
 {
-    public class ETradeDbContext : DbContext
+    public class ETradeDbContext : IdentityDbContext<AppUser,IdentityRole,string>
     {
         public ETradeDbContext(DbContextOptions<ETradeDbContext> options) : base(options)
         {
@@ -13,6 +15,7 @@ namespace E_Trade.Repository
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<UserRefreshToken> UserRefreshTokens { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {

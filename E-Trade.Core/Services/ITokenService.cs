1d43a10b697649c5649ae14ccb975009972d16b6
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-04-15 15:58:31 +0300

    Core Layer: Added ITokenService interface.

---
+++ E-Trade.Core/Services/ITokenService.cs
@@ -0,0 +1,18 @@
+﻿using E_Trade.Core.DTOs;
+using E_Trade.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Trade.Core.Services
+{
+    // Token üretecek service class'ın implement edeceği interface
+
+    // ITokenService interface
+    public interface ITokenService
+    {
+        TokenDto CreateToken(AppUser appUser);
+    }
+}

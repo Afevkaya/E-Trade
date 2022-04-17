15780c489f48ce910dc355cf987f6c28efac0997
Afevkaya <Afevkaya5151@gmail.com>
2022-04-17 14:42:30 +0300

    Service Layer : Added SignService class.

---
+++ E-Trade.Service/Services/SignService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace E_Trade.Service.Services
+{
+    // Access Token 'ı imzalamak için gerekli string ifadeyi imzalayan class. 
+
+    // SignService class
+    public static class SignService
+    {
+        public static SecurityKey GetSymmetricSecurityKey(string securityKey)
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+        }
+    }
+}

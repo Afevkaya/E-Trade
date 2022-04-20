e66831e053f33dfdd657edb6163775a80eaecdc3
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-04-20 14:58:35 +0300

    Service Layer: Added comment lines to Mapping class.

--- E-Trade.Service/Mapping/MapProfile.cs
+++ E-Trade.Service/Mapping/MapProfile.cs
@@ -4,6 +4,14 @@ using E_Trade.Core.Models;
 
 namespace E_Trade.Service.Mapping
 {
+    // Entity classlarının Dto classlarına ya da Dto classlarının Entity classlarına maplendiği class.
+    // Db tarafından gelen ya da Db tarafına yansıyacak class Entity class.
+    // Kullanıcıya gösterilecek veya kullanıcıdan gelecek class Dto class.
+    // Bu yüzden mapleme işlemi yapılır.
+    // AutoMapper pakedini yüklemek gerekli.
+    // Profile class'ını inherit edilmeli.
+
+    // MapProfile class
     public class MapProfile : Profile
     {
         public MapProfile()

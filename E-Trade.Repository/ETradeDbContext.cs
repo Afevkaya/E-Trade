726a6ee4c29948844c691b1eaa889af39cb488c9
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-04-20 11:10:26 +0300

    Repository Layer: Added comment lines to ETradeDbContext class.

--- E-Trade.Repository/ETradeDbContext.cs
+++ E-Trade.Repository/ETradeDbContext.cs
@@ -6,18 +6,25 @@ using System.Reflection;
 
 namespace E_Trade.Repository
 {
+    // Database tabloları ile entity class'larını birbirine maplediğimiz class.
+    // Identity işlemleri kullanılacağı için IdentityDbContext class'ından inheritance alınıyor.
+
+    // ETradeDbContext class
     public class ETradeDbContext : IdentityDbContext<AppUser,IdentityRole,string>
     {
+        // Db config işlemleri
         public ETradeDbContext(DbContextOptions<ETradeDbContext> options) : base(options)
         {
 
         }
 
+        // Maplenilen bölüm
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<UserRefreshToken> UserRefreshTokens { get; set; }
 
 
+        // Db tablo config işlemleri
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());

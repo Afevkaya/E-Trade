10273bddb98774dbeb2bb3bd3aa9002697a16a72
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-04-20 14:52:35 +0300

    Service Layer: Added comment lines to Validators file.

--- E-Trade.Service/Validations/CategoryDtoValidator.cs
+++ E-Trade.Service/Validations/CategoryDtoValidator.cs
@@ -3,6 +3,11 @@ using FluentValidation;
 
 namespace E_Trade.Service.Validations
 {
+    // CategoryDto class için validasyon işlemlerini yapıldığı class.
+    // Validasyon işlemlerinin kullanılabilmesi için Fluent Validasion pakedinin olması gerekli.
+    // Validasyon class olabilmesi için AbstractValidator<> generic abstarc class'ını inherit etmesi gerekli.
+
+    // CategoryDtoValidator class.
     public class CategoryDtoValidator : AbstractValidator<CategoryDto>
     {
         public CategoryDtoValidator()

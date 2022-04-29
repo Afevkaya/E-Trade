75cd8f5855a0ba99ba09cbfeb4c734c8a3d909a3
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-04-29 16:17:23 +0300

    Service Layer: Added BasketService class.

---
+++ E-Trade.Service/Services/BasketService.cs
@@ -0,0 +1,19 @@
+﻿using E_Trade.Core.Models;
+using E_Trade.Core.Repositories;
+using E_Trade.Core.Services;
+using E_Trade.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Trade.Service.Services
+{
+    public class BasketService : Service<Basket>, IBasketService
+    {
+        public BasketService(IUnitOfWork unitOfWork, IGenericRepository<Basket> repository) : base(unitOfWork, repository)
+        {
+        }
+    }
+}

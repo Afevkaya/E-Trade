9dfdd2eefe0f0a0c9a92ea88f6463cc5e82f6db3
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-04-29 16:16:25 +0300

    Core Layer: Added IBasketService interface.

---
+++ E-Trade.Core/Services/IBasketService.cs
@@ -0,0 +1,13 @@
+﻿using E_Trade.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Trade.Core.Services
+{
+    public interface IBasketService : IService<Basket>
+    {
+    }
+}

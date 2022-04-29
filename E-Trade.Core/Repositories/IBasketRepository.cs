3b9167fa7f57e717fed4d8efb03abb8f0f3360a1
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-04-29 16:15:38 +0300

    Core Layer: Added IBasketRepoistory interface.

---
+++ E-Trade.Core/Repositories/IBasketRepository.cs
@@ -0,0 +1,13 @@
+﻿using E_Trade.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Trade.Core.Repositories
+{
+    public interface IBasketRepository : IGenericRepository<Basket>
+    {
+    }
+}

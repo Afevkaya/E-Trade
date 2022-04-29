1ee35141451df859548461ea30e3a6d06eff9a84
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-04-29 16:13:51 +0300

    Core Layer: Added Basket model class.

---
+++ E-Trade.Core/Models/Basket.cs
@@ -0,0 +1,11 @@
+﻿namespace E_Trade.Core.Models
+{
+    public class Basket
+    {
+        public int Id { get; set; }
+        public AppUser AppUser { get; set; }
+        public string AppUserId { get; set; }
+        public Product Product { get; set; }
+        public int ProductId { get; set; }
+    }
+}

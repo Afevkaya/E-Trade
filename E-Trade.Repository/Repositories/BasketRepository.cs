0c0acd3256f4c2d4e20003ba40e913b15b622c50
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-05-12 11:53:36 +0300

    Repo Layer: Added BasketRepository class.

---
+++ E-Trade.Repository/Repositories/BasketRepository.cs
@@ -0,0 +1,21 @@
+﻿using E_Trade.Core.Models;
+using E_Trade.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace E_Trade.Repository.Repositories
+{
+    public class BasketRepository : GenericRepository<Basket>, IBasketRepository
+    {
+        private readonly DbSet<Basket> _basketSet;
+        public BasketRepository(ETradeDbContext dbContext) : base(dbContext)
+        {
+            _basketSet = dbContext.Set<Basket>();
+        }
+
+        public async Task<List<Basket>> GetAllAsync()
+        {
+            return await _basketSet.ToListAsync();
+        }
+    }
+}

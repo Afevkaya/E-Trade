83bafe09ecdc82a50f07c2a7fa8c833460c2885f
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-05-12 14:40:56 +0300

    Service Layer: Added validation rules of CreateBasketDtoValidator class.

---
+++ E-Trade.Service/Validations/CreateBasketDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using E_Trade.Core.DTOs;
+using FluentValidation;
+
+namespace E_Trade.Service.Validations
+{
+    public class CreateBasketDtoValidator : AbstractValidator<CreateBasketDto>
+    {
+        public CreateBasketDtoValidator()
+        {
+            RuleFor(x => x.AppUserId).NotNull().WithMessage("{PropertyName} alanı null olamaz").NotEmpty().WithMessage("{PropertyName} alanı boş olamaz");
+            RuleFor(x => x.ProductId).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} alanı minimum 1 olmalıdır");
+            RuleFor(x => x.ProductQuantity).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} alanı minimum 1 olmalıdır");
+            RuleFor(x => x.ProductPrice).InclusiveBetween(1, decimal.MaxValue).WithMessage("{PropertyName} alanı minimum 1 olmalıdır");
+        }
+    }
+}

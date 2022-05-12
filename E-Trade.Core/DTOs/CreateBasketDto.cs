a09da5bbfc25f63d66885b1256dfef6158bcebad
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-05-12 11:54:37 +0300

    Core Layer: Added CreateBasketDto and ResponseBasketDto classes

---
+++ E-Trade.Core/DTOs/CreateBasketDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Trade.Core.DTOs
+{
+    public class CreateBasketDto
+    {
+        public int ProductId { get; set; }
+        public string AppUserId { get; set; }
+        public int ProductQuantity { get; set; }
+        public decimal ProductPrice { get; set; }
+
+    }
+}
---
+++ E-Trade.Core/DTOs/ResponseBasketDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Trade.Core.DTOs
+{
+    public class ResponseBasketDto
+    {
+        public string ProductName { get; set; }
+        public string AppUserName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int ProductQuantity { get; set; }
+        public decimal Total { get; set; }
+
+    }
+}

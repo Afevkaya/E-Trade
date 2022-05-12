b1f20a57361aa2f3c0976e60a0395b12d2a82983
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-05-12 11:55:05 +0300

    Core Layer: Added IBasketService interface.

---
+++ E-Trade.Core/Services/IBasketService.cs
@@ -0,0 +1,16 @@
+﻿using E_Trade.Core.DTOs;
+using E_Trade.Core.Models;
+using System.Linq.Expressions;
+
+namespace E_Trade.Core.Services
+{
+    public interface IBasketService
+    {
+        Task<CustomResponseDto<IEnumerable<ResponseBasketDto>>> GetAllAsync();
+        Task<CustomResponseDto<IEnumerable<ResponseBasketDto>>> Where(Expression<Func<Basket, bool>> expression);
+        Task<CustomResponseDto<ResponseBasketDto>> GetByIdAsync(int id);
+        Task<CustomResponseDto<ResponseBasketDto>> AddAsync(CreateBasketDto basketDto);
+        Task<CustomResponseDto<NoContentDto>> RemoveAsync(int id);
+
+    }
+}

7eff6abfe3e35512e94bff02d030160d7312fc2b
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-05-12 17:38:47 +0300

    Service Layer: The AddAsyncTwo method has been coded.

--- E-Trade.Service/Services/ProductService.cs
+++ E-Trade.Service/Services/ProductService.cs
@@ -16,12 +16,48 @@ namespace E_Trade.Service.Services
     {
         private readonly IProductRepository _productRepository;
         private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
         public ProductService(IUnitOfWork unitOfWork, IGenericRepository<Product> repository, IProductRepository productRepository, IMapper mapper) : base(unitOfWork, repository)
         {
             _productRepository = productRepository;
             _mapper = mapper;
+            _unitOfWork = unitOfWork;
         }
-        
+
+        public async Task<CustomResponseDto<ProductDto>> AddAsyncTwo(ProductDto productDto)
+        {
+
+            Product product;
+            if(productDto.Id > 0)
+            {
+                var products = _productRepository.GetAll();
+                var oldProduct = products.FirstOrDefault(p => p.Id == productDto.Id);
+                product = oldProduct;
+                product.StockQuantity = productDto.Quantity;
+                _productRepository.Update(product);
+            }
+            else
+            {
+                product = _mapper.Map<Product>(productDto);
+                await _productRepository.AddAsync(product);
+            }
+            
+            await _unitOfWork.CommitAsync();
+
+            var newProductDto = new ProductDto
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Price = product.Price,
+                Description = product.Description,
+                ImageUrl = product.ImageUrl,
+                Quantity = product.StockQuantity,
+                CategoryId = product.CategoryId,
+            };
+            return CustomResponseDto<ProductDto>.Success(200, newProductDto);
+        }
+
         /*
         public async Task<List<ProductsWithCategoryDto>> GetProductsWithCategory()
         {
@@ -30,7 +66,7 @@ namespace E_Trade.Service.Services
             return productsWithCategoryDto;
         }
         */
-        
+
         public async Task<CustomResponseDto<List<ProductsWithCategoryDto>>> GetProductsWithCategory()
         {
             var products = await _productRepository.GetProductsWithCategory();

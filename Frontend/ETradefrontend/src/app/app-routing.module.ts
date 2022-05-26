164878e87702be3d9b649f5dfd458e31be652898
Afevkaya <Afevkaya5151@gmail.com>
2022-05-26 13:52:15 +0300

    Creation and testing of the routing mechanism.

--- Frontend/ETradefrontend/src/app/app-routing.module.ts
+++ Frontend/ETradefrontend/src/app/app-routing.module.ts
@@ -1,7 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth-pages/login/login.component';
+import { SigninComponent } from './auth-pages/signin/signin.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    // www.etrade.com
+    path:"",
+    component: MainLayoutComponent
+  },
+
+  {
+    // www.etrade.com/auth
+    path:"auth",
+    component:AuthLayoutComponent,
+    children: [
+      {
+        // www.etrade.com/auth
+        path: "",
+        component: LoginComponent,
+      },
+
+      {
+        // www.etrade.com/auth/login
+        path: "login",
+        component: LoginComponent,
+      },
+
+      {
+        // www.etrade.com/auth/signin
+        path: "signin",
+        component: SigninComponent
+      },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/ETradefrontend/src/app/app.module.ts
+++ Frontend/ETradefrontend/src/app/app.module.ts
@@ -7,8 +7,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
-import { SigninComponent } from './auth-pages/signin/signin.component';
 import { LoginComponent } from './auth-pages/login/login.component';
+import { SigninComponent } from './auth-pages/signin/signin.component';
 
 @NgModule({
   declarations: [
@@ -16,8 +16,8 @@ import { LoginComponent } from './auth-pages/login/login.component';
     AuthLayoutComponent,
     AdminLayoutComponent,
     MainLayoutComponent,
-    SigninComponent,
     LoginComponent,
+    SigninComponent,
   ],
   imports: [
     BrowserModule,

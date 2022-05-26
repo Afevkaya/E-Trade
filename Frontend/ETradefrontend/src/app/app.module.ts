0a5ec5685380f1967eeca8a46ed7da753cf37adf
Afevkaya <Afevkaya5151@gmail.com>
2022-05-26 11:01:21 +0300

    Adding signin-pages and login-pages components to the project.

--- Frontend/ETradefrontend/src/app/app.module.ts
+++ Frontend/ETradefrontend/src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { AppComponent } from './app.component';
-
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { SigninComponent } from './auth-pages/signin/signin.component';
+import { LoginComponent } from './auth-pages/login/login.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,8 @@ import { MainLayoutComponent } from './layouts/main-layout/main-layout.component
     AuthLayoutComponent,
     AdminLayoutComponent,
     MainLayoutComponent,
+    SigninComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,

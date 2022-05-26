5875d9b3d4deec1e7e695857fece071a0992b8e7
Afevkaya <Afevkaya5151@gmail.com>
2022-05-26 17:18:13 +0300

    Removing the material.module module.

--- Frontend/ETradefrontend/src/app/app.module.ts
+++ Frontend/ETradefrontend/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { LoginComponent } from "./auth-pages/login/login.component";
+import { SigninComponent } from "./auth-pages/signin/signin.component";
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
-import { LoginComponent } from './auth-pages/login/login.component';
-import { SigninComponent } from './auth-pages/signin/signin.component';
+import { HomeComponent } from './main-pages/home/home.component';
+import { MyaccountComponent } from './main-pages/myaccount/myaccount.component';
 
 @NgModule({
   declarations: [
@@ -18,11 +21,14 @@ import { SigninComponent } from './auth-pages/signin/signin.component';
     MainLayoutComponent,
     LoginComponent,
     SigninComponent,
+    HomeComponent,
+    MyaccountComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

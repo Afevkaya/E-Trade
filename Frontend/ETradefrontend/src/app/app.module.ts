7be7612e9766797af87442484f7ec395805946a6
Afevkaya <Afevkaya5151@gmail.com>
2022-05-26 10:13:27 +0300

    adding layouts to the project.

--- Frontend/ETradefrontend/src/app/app.module.ts
+++ Frontend/ETradefrontend/src/app/app.module.ts
@@ -4,17 +4,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from "@angular/material/slider";
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AuthLayoutComponent,
+    AdminLayoutComponent,
+    MainLayoutComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]

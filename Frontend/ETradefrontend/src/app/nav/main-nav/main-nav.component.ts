9de502607e6427ed18eeef53ee304ce14c7aaa18
Afevkaya <Afevkaya5151@gmail.com>
2022-05-27 11:16:43 +0300

    Adding active class to main-nav component links.

--- Frontend/ETradefrontend/src/app/nav/main-nav/main-nav.component.ts
+++ Frontend/ETradefrontend/src/app/nav/main-nav/main-nav.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
-  styleUrls: ['./main-nav.component.css']
+  styleUrls: ['./main-nav.component.css'],
 })
 export class MainNavComponent implements OnInit {
+  pageActive?: MainPage;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    this.router.events.subscribe((x) => {
+      if (x instanceof NavigationEnd) {
+        if (x.url.indexOf('anasayfa') > 0) {
+          this.pageActive = MainPage.home;
+        } else if (x.url.indexOf('myaccount') > 0) {
+          this.pageActive = MainPage.myaccount;
+        } else {
+          this.pageActive = MainPage.home;
+        }
+      }
+    });
   }
 
+  ngOnInit(): void {}
+}
+
+enum MainPage {
+  home = 1,
+  myaccount = 2,
 }

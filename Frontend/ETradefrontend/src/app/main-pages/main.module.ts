91fdacd61ecb0e4d130f68dbedfe74d1fa6b226b
Afevkaya <Afevkaya5151@gmail.com>
2022-05-29 18:13:29 +0300

    Adding the main module to the project.

---
+++ Frontend/ETradefrontend/src/app/main-pages/main.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule } from "../app-routing.module";
+import { ComponentsModule } from "../components/components.module";
+
+import { MainLayoutComponent } from '../layouts/main-layout/main-layout.component';
+import { MainNavComponent } from '../nav/main-nav/main-nav.component';
+import { HomeComponent } from './home/home.component';
+import { MyaccountComponent } from './myaccount/myaccount.component';
+
+
+
+@NgModule({
+  declarations: [MainLayoutComponent,MainNavComponent,HomeComponent,MyaccountComponent],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ComponentsModule
+  ]
+})
+export class MainModule { }
+
+// components.module > main.module > app.module

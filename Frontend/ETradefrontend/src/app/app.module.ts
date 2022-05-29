01d7f62cdbf28784f49150b4f773ec24ae291b61
Afevkaya <Afevkaya5151@gmail.com>
2022-05-29 18:14:25 +0300

    Editing the app.module module.

--- Frontend/ETradefrontend/src/app/app.module.ts
+++ Frontend/ETradefrontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from "@angular/common/http";
+import { MainModule } from './main-pages/main.module';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,32 +10,25 @@ import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component
 import { LoginComponent } from "./auth-pages/login/login.component";
 import { SigninComponent } from "./auth-pages/signin/signin.component";
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
-import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
-import { HomeComponent } from './main-pages/home/home.component';
-import { MyaccountComponent } from './main-pages/myaccount/myaccount.component';
-import { MainNavComponent } from './nav/main-nav/main-nav.component';
+
 import { ProductService } from './services/product.service';
 import { CategoryService } from './services/category.service';
-import { MenuCategoryComponent } from './components/menu-category/menu-category.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthLayoutComponent,
     AdminLayoutComponent,
-    MainLayoutComponent,
     LoginComponent,
     SigninComponent,
-    HomeComponent,
-    MyaccountComponent,
-    MainNavComponent,
-    MenuCategoryComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    MainModule
   ],
   providers: [ProductService,CategoryService],
   bootstrap: [AppComponent]

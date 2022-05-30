d4a580b299a74065ed4f8e658e844214539d7345
Afevkaya <Afevkaya5151@gmail.com>
2022-05-30 14:22:57 +0300

    Adding the Products Component to the project.

--- Frontend/ETradefrontend/src/app/components/components.module.ts
+++ Frontend/ETradefrontend/src/app/components/components.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from "@angular/router";
+import { RouterModule } from '@angular/router';
 
 import { MenuCategoryComponent } from './menu-category/menu-category.component';
 import { PageTitleComponent } from './page-title/page-title.component';
-
-
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
-  declarations: [MenuCategoryComponent, PageTitleComponent],
-  imports: [
-    CommonModule,
-    RouterModule
-  ],
-  exports: [MenuCategoryComponent, PageTitleComponent]
+  declarations: [MenuCategoryComponent, PageTitleComponent, ProductsComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [MenuCategoryComponent, PageTitleComponent, ProductsComponent],
 })
-export class ComponentsModule { }
+export class ComponentsModule {}
---
+++ Frontend/ETradefrontend/src/app/components/products/products.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css'],
+})
+export class ProductsComponent implements OnInit {
+  @Input() products?: Product[];
+
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}

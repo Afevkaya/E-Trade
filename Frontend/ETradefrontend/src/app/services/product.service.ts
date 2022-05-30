72bde014913a7ee28dd824f331ea90444230e2fe
Afevkaya <Afevkaya5151@gmail.com>
2022-05-30 11:36:35 +0300

    Updating the ProductService.ts file.

--- Frontend/ETradefrontend/src/app/services/product.service.ts
+++ Frontend/ETradefrontend/src/app/services/product.service.ts
@@ -1,26 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { tap } from "rxjs/operators";
 
 @Injectable()
 export class ProductService {
 
+  public loading:boolean = true;
   private apiUrl: string = "https://localhost:7192/api/products";
 
 
-  constructor(private httpClient:HttpClient)
-  {
-
-  }
+  constructor(private httpClient:HttpClient){ }
 
   // Tüm productları getirme
   // Json tipinde bir obje dönüyor.
+  // loading değişkeni datanın gelip gelmesiğini kontrol ediyor.
   getProducts(){
-    return this.httpClient.get<any>(this.apiUrl);
-  }
-
-  // id'ye göre product getirme
-  getProductsById(id:number){
-    let newApiUrl = `${this.apiUrl}/${id}`;
-    return this.httpClient.get<any>(newApiUrl);
+    this.httpClient.get<any>(this.apiUrl).pipe(tap(x=>{
+      this.loading = false;
+    }))
   }
 }

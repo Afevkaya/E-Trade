74cad6b9d98a47d32fc4cc59a97d9efbb709df7a
Afevkaya <Afevkaya5151@gmail.com>
2022-05-31 19:07:02 +0300

    Creation and coding of the Product Component.

--- Frontend/ETradefrontend/src/app/main-pages/product/product.component.ts
+++ Frontend/ETradefrontend/src/app/main-pages/product/product.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Category } from 'src/app/models/category';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  product!:Product;
+  category!:Category;
+  constructor(public productService:ProductService,private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params=>{
+      this.productService.loading = true;
+      let id = Number(this.route.snapshot.paramMap.get("id"));
+      this.productService.getProduct(id).subscribe(result=>{
+        this.product = result.data;
+        this.category = result.data.category;
+      });
+    });
   }
 
 }

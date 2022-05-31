1c2c1721c488ef7b3fa054e05ca6de3f03cba053
Afevkaya <Afevkaya5151@gmail.com>
2022-05-31 17:24:49 +0300

    Updating the product and category model classes.

--- Frontend/ETradefrontend/src/app/models/product.ts
+++ Frontend/ETradefrontend/src/app/models/product.ts
@@ -1,9 +1,12 @@
+import { Category } from "./category";
+
 export class Product {
-  id?:number;
-  name?:string;
-  price?:number;
-  description?:string;
-  imageUrl?:string;
-  stockQuantity?:number;
-  categoryId?:number;
+  id!:number;
+  name!:string;
+  price!:number;
+  description!:string;
+  imageUrl!:string;
+  stockQuantity!:number;
+  categoryId!:number;
+  category!:Category;
 }

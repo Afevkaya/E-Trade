f53c36daad961051f64bfda91cacf919c8fa236d
Afevkaya <Afevkaya5151@gmail.com>
2022-06-01 10:44:24 +0300

    Coding ngOnDestroy method in HomeComponent ts file.

--- Frontend/ETradefrontend/src/app/main-pages/home/home.component.ts
+++ Frontend/ETradefrontend/src/app/main-pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -7,16 +7,25 @@ import { ProductService } from 'src/app/services/product.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   products: Product[] = [];
+  ajax: any;
+
   constructor(private productService: ProductService) { }
 
+
   ngOnInit(): void {
-    this.productService.getProducts().subscribe(result=>{
+    this.ajax = this.productService.getProducts().subscribe(result=>{
       console.log(result.data);
       this.products = result.data;
     });
+  }
+
+  ngOnDestroy(): void {
+    if (this.ajax != null) {
+      this.ajax.unsubscribe();
+    }
   }
 
 }

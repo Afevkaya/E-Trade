ef61a2b8b7156ff5ea9b38f4bde9a8830f5f66a2
Afevkaya <Afevkaya5151@gmail.com>
2022-06-01 14:54:10 +0300

    Encoding the relevant method in the Category service.

--- Frontend/ETradefrontend/src/app/services/category.service.ts
+++ Frontend/ETradefrontend/src/app/services/category.service.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Category } from "../models/category";
+import { tap } from 'rxjs/operators';
+import { ProductService } from './product.service';
 
 @Injectable()
 export class CategoryService {
 
   private apiUrl: string = "https://localhost:7192/api/categories";
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient,private productService:ProductService) { }
 
   // Backend tarafında bir yapı dönüyor.
   // Bu yapı içinde success ise data değilse error geliyor.
   // Bu yüzden dönüş tipi any
   getCategories(){
     return this.httpClient.get<any>(this.apiUrl);
+  }
+
+  getCategoryWithProducts(categoryId:number){
+    let newApiUrl = `${this.apiUrl}/GetCategoryWithProducts/${categoryId}`;
+    return this.httpClient.get<any>(newApiUrl).pipe(tap(x=>{
+      this.productService.loading = false;
+    }));
   }
 }

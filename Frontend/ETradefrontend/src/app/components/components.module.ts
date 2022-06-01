e729b93400aa51a221cc410714b77e6c21ce21d5
Afevkaya <Afevkaya5151@gmail.com>
2022-06-01 10:16:37 +0300

    Adding the urlformat pipe to the components module.

--- Frontend/ETradefrontend/src/app/components/components.module.ts
+++ Frontend/ETradefrontend/src/app/components/components.module.ts
@@ -5,10 +5,11 @@ import { RouterModule } from '@angular/router';
 import { MenuCategoryComponent } from './menu-category/menu-category.component';
 import { PageTitleComponent } from './page-title/page-title.component';
 import { ProductsComponent } from './products/products.component';
+import { UrlformatPipe } from '../pipes/urlformat.pipe';
 
 @NgModule({
-  declarations: [MenuCategoryComponent, PageTitleComponent, ProductsComponent],
+  declarations: [MenuCategoryComponent, PageTitleComponent, ProductsComponent, UrlformatPipe],
   imports: [CommonModule, RouterModule],
-  exports: [MenuCategoryComponent, PageTitleComponent, ProductsComponent],
+  exports: [MenuCategoryComponent, PageTitleComponent, ProductsComponent, UrlformatPipe],
 })
 export class ComponentsModule {}

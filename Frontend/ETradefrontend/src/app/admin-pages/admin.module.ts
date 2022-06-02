80cd89f5b77e146503f6b99b710f1bebaff85551
Afevkaya <Afevkaya5151@gmail.com>
2022-06-02 11:49:15 +0300

    Adding admin components to the admin.module file.

--- Frontend/ETradefrontend/src/app/admin-pages/admin.module.ts
+++ Frontend/ETradefrontend/src/app/admin-pages/admin.module.ts
@@ -6,9 +6,14 @@ import { ComponentsModule } from "../components/components.module";
 
 import { AdminLayoutComponent } from "../layouts/admin-layout/admin-layout.component";
 import { AdminNavComponent } from "../nav/admin-nav/admin-nav.component";
+import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { AdminProductComponent } from './product/admin-product/admin-product.component';
+import { AdminProductListComponent } from './product/admin-product-list/admin-product-list.component';
+import { AdminProductAddComponent } from './product/admin-product-add/admin-product-add.component';
+import { AdminProductUpdateComponent } from './product/admin-product-update/admin-product-update.component';
 
 @NgModule({
-  declarations: [AdminLayoutComponent, AdminNavComponent],
+  declarations: [AdminLayoutComponent, AdminNavComponent, AdminHomeComponent, AdminProductComponent, AdminProductListComponent, AdminProductAddComponent, AdminProductUpdateComponent],
   imports: [
     CommonModule, AppRoutingModule, MaterialModule, ComponentsModule
   ]

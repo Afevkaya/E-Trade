5612a12f22993eb46826d5c78dd9b4c102435fa8
Afevkaya <Afevkaya5151@gmail.com>
2022-06-03 16:26:49 +0300

    Coding the admin-add-product component.

--- Frontend/ETradefrontend/src/app/admin-pages/product/admin-product-add/admin-product-add.component.ts
+++ Frontend/ETradefrontend/src/app/admin-pages/product/admin-product-add/admin-product-add.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Category } from 'src/app/models/category';
+import { CategoryService } from 'src/app/services/category.service';
+import { MyvalidationService } from 'src/app/services/myvalidation.service';
+import { ProductService } from 'src/app/services/product.service';
 @Component({
   selector: 'app-admin-product-add',
   templateUrl: './admin-product-add.component.html',
@@ -7,9 +12,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminProductAddComponent implements OnInit {
 
-  constructor() { }
+  productForm!:FormGroup;
+  categories!:Category[];
+  success!:boolean;
+  loading!:boolean;
+  info!:string;
+  constructor(private productService:ProductService,private categoryService:CategoryService, public myValidationService:MyvalidationService, private router:Router) { }
 
   ngOnInit(): void {
+    this.getCategory();
+    this.productForm = new FormGroup({
+      name: new FormControl("",[Validators.required,Validators.minLength(5),Validators.maxLength(String.length)]),
+      price: new FormControl("",[Validators.required,Validators.min(1),Validators.max(Number.MAX_SAFE_INTEGER),Validators.pattern("^[0-9]*$")]),
+      description: new FormControl("",[Validators.required,Validators.minLength(50),Validators.maxLength(String.length)]),
+      imageUrl: new FormControl("",Validators.required),
+      stockQuantity: new FormControl("",[Validators.required,Validators.min(1),Validators.max(Number.MAX_SAFE_INTEGER),Validators.pattern("^[0-9]*$")]),
+      categoryId: new FormControl("",Validators.required)
+    });
   }
+
+  get getControls(){
+    return this.productForm.controls;
+  }
+
+  onSubmit(){
+    this.loading = true;
+    this.productService.addProduct(this.productForm.value).subscribe(result => {
+      this.success = true;
+      this.router.navigateByUrl('/admin/urun/liste')
+    },
+    error => {
+      this.success = false;
+      this.info = "bir hata meydana geldi " +error.message;
+      console.log(error.message);
+    })
+  }
+
+  getCategory(){
+    this.categoryService.getCategories().subscribe(result=>{
+      this.categories = result.data;
+    },error => {
+
+    })
+  }
+
 
 }

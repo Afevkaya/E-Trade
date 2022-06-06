058c3c7471bf2c6d9e4f611300ad0ee09140515a
Afevkaya <Afevkaya5151@gmail.com>
2022-06-06 10:34:29 +0300

    Updating the myvalidation.service file.

--- Frontend/ETradefrontend/src/app/services/myvalidation.service.ts
+++ Frontend/ETradefrontend/src/app/services/myvalidation.service.ts
@@ -21,12 +21,12 @@ export class MyvalidationService {
           return `${name} alanı en az 5 karakter olmalıdır`;
         }
         else if(errorname == "maxlength"){
-          return `${name} alanı en fazla ${String.length} karakter olmalıdır.`
+          return `${name} alanı en fazla 400 karakter olmalıdır.`
         }
         else if(errorname == "min"){
           return `${name} değeri en az 1 olmalıdır.`
         }
-        else if(errorname == "maxlength"){
+        else if(errorname == "max"){
           return `${name} değeri en fazla ${Number.MAX_SAFE_INTEGER} olmalıdır.`
         }
         else if(errorname == "pattern"){

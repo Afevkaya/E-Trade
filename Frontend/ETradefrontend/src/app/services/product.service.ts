3ed89f5357866f39eab1ddd0050f3c6ad3d9da0e
Afevkaya <Afevkaya5151@gmail.com>
2022-06-06 10:33:40 +0300

    Adding the updateProduct method to the product.service file.

--- Frontend/ETradefrontend/src/app/services/product.service.ts
+++ Frontend/ETradefrontend/src/app/services/product.service.ts
@@ -20,24 +20,28 @@ export class ProductService {
       this.loading = false;
     }));
   }
+
   getProduct(id:number){
     let newApiUrl = `${this.apiUrl}/${id}`;
     return this.httpClient.get<any>(newApiUrl).pipe(tap(x=>{
       this.loading = false;
     }));
   }
+
   getProductByCategory(id:number){
     let newApiUrl = `${this.apiUrl}/${id}`;
     return this.httpClient.get<any>(newApiUrl).pipe(tap(x=>{
       this.loading = false;
     }));
   }
+
   getSearchProduct(searchText:string){
     let newApiUrl = `${this.apiUrl}/Search/${searchText}`;
     return this.httpClient.get<any>(newApiUrl).pipe(tap(x=>{
       this.loading = false;
     }));
   }
+
   getProductsWithCategory(){
     let newApiUrl = `${this.apiUrl}/ProductsWithCategory`
     return this.httpClient.get<any>(newApiUrl);
@@ -45,5 +49,9 @@ export class ProductService {
 
   addProduct(product:Product){
     return this.httpClient.post<any>(`${this.apiUrl}`,product);
+  }
+
+  updateProduct(product:Product){
+    return this.httpClient.put<any>(`${this.apiUrl}`,product);
   }
 }

ecfc8b52a5bf3a45e399a4ec2571e2fc582df774
Afevkaya <Afevkaya5151@gmail.com>
2022-06-08 16:13:15 +0300

    Updating the admin-login component.

--- Frontend/ETradefrontend/src/app/auth-pages/admin-login/admin-login.component.ts
+++ Frontend/ETradefrontend/src/app/auth-pages/admin-login/admin-login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Login } from 'src/app/models/login';
 import { AuthService } from 'src/app/services/auth.service';
+import { SigninvalidationService } from 'src/app/services/signinvalidation.service';
 
 @Component({
   selector: 'app-admin-login',
@@ -10,25 +11,41 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class AdminLoginComponent implements OnInit {
 
-  loginModel!:Login;
-  constructor(private authService:AuthService,private router:Router) { }
+  // propertyler
+  loginForm!:FormGroup;
+  loading!:boolean;
+  success!:boolean;
+  info!:string;
+  hide = true;
+
+  // Kullanılacak serviceler ve moduller
+  constructor(private authService:AuthService,private router:Router, public signinValidation:SigninvalidationService) { }
 
   ngOnInit(): void {
+    // Reactive form
+    this.loginForm = new FormGroup({
+      email: new FormControl('',[Validators.required,Validators.email]),
+      password: new FormControl('',Validators.required)
+    })
   }
 
-  login(email:string,password:string){
-    this.loginModel = new Login();
-    this.loginModel.email = email;
-    this.loginModel.password = password;
-
-    this.authService.createToken(this.loginModel).subscribe(result=>{
-      if(result.data != null){
-        localStorage.setItem("accessToken",result.data.accessToken);
-        localStorage.setItem("refreshToken",result.data.refreshToken);
-        localStorage.setItem("accessTokenExpiration",result.data.accessTokenExpiration);
-        localStorage.setItem("refreshTokenExpiration",result.data.refreshTokenExpiration);
-        this.router.navigate(["/admin"]);
-      }
-    });
+  login(){
+    // form valid kontrol
+    if(this.loginForm.valid){
+      this.loading = true;
+      // login ol
+      this.authService.createToken(this.loginForm.value).subscribe(result=>{
+        if(result.data != null){
+          localStorage.setItem("accessToken",result.data.accessToken);
+          localStorage.setItem("refreshToken",result.data.refreshToken);
+          localStorage.setItem("accessTokenExpiration",result.data.accessTokenExpiration);
+          localStorage.setItem("refreshTokenExpiration",result.data.refreshTokenExpiration);
+          this.router.navigate(["/admin"]);
+        }
+      },error =>{
+        this.success = false;
+        this.info = 'Bir hata meydana geldi '+error.message;
+      });
+    }
   }
 }

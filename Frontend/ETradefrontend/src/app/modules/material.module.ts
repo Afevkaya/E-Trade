4abc6abd57cc4cae3bb083f89433506622487cbe
Afevkaya <Afevkaya5151@gmail.com>
2022-06-08 14:18:17 +0300

    Adding the necessary modules to the material.module file.

--- Frontend/ETradefrontend/src/app/modules/material.module.ts
+++ Frontend/ETradefrontend/src/app/modules/material.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
-import { MatTableModule } from "@angular/material/table";
-import { MatPaginatorModule } from '@angular/material/paginator'
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
 @NgModule({
   declarations: [],
   imports: [
@@ -16,6 +18,8 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
     FormsModule,
     MatButtonModule,
     MatInputModule,
+    MatIconModule,
+    MatRadioModule,
   ],
   exports: [
     CommonModule,
@@ -28,7 +32,9 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
     MatInputModule,
     MatTableModule,
     MatPaginatorModule,
-    MatAutocompleteModule
+    MatAutocompleteModule,
+    MatIconModule,
+    MatRadioModule,
   ],
 })
 export class MaterialModule {}

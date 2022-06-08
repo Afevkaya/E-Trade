1e5384dae19fd8b3afb6f112d6263db4947b58bd
Afevkaya <Afevkaya5151@gmail.com>
2022-06-08 10:10:22 +0300

    Encoding the isAuthenticated method in the auth.service file.

--- Frontend/ETradefrontend/src/app/services/auth.service.ts
+++ Frontend/ETradefrontend/src/app/services/auth.service.ts
@@ -1,14 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { Login } from '../models/login';
 
 @Injectable()
 export class AuthService {
 
   private apiUrl: string = 'https://localhost:7192/api/auths';
-  constructor(private httpClient:HttpClient) {}
+  constructor(private httpClient:HttpClient, public jwtHelper:JwtHelperService) {}
 
   createToken(login:Login){
     return this.httpClient.post<any>(`${this.apiUrl}/CreateToken`,login);
   }
+
+  public isAuthenticated(): boolean {
+    const accessToken = localStorage.getItem('accessToken') || '';
+    return !this.jwtHelper.isTokenExpired(accessToken);
+  }
+
+  // isLoggedIn(){
+  //   return localStorage.getItem('accessToken')!=null;
+  // }
 }

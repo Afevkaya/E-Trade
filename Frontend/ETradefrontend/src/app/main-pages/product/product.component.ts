48454b91643cfb22e6e4eb64721407a69440e1e9
Afevkaya <Afevkaya5151@gmail.com>
2022-06-09 17:45:53 +0300

    Coding getByUserId and deleteBasket methods in basket service.

--- Frontend/ETradefrontend/src/app/main-pages/product/product.component.ts
+++ Frontend/ETradefrontend/src/app/main-pages/product/product.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { Basket } from 'src/app/models/basket';
 import { Category } from 'src/app/models/category';
@@ -10,29 +10,33 @@ import { ProductService } from 'src/app/services/product.service';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-
-  product!:Product;
-  category!:Category;
+  product!: Product;
+  category!: Category;
   basket!: Basket;
-  productQuantity:number = 1;
-  constructor(public productService:ProductService,private route: ActivatedRoute,private helperService:JwtHelperService,private basketService:BasketService) { }
+  productQuantity: number = 1;
+  constructor(
+    public productService: ProductService,
+    private route: ActivatedRoute,
+    private helperService: JwtHelperService,
+    private basketService: BasketService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params=>{
+    this.route.paramMap.subscribe((params) => {
       this.productService.loading = true;
-      let id = Number(this.route.snapshot.paramMap.get("id"));
-      this.productService.getProduct(id).subscribe(result=>{
+      let id = Number(this.route.snapshot.paramMap.get('id'));
+      this.productService.getProduct(id).subscribe((result) => {
         this.product = result.data;
         this.category = result.data.category;
       });
     });
   }
 
-  add(productId:number, productPrice:number){
-    debugger
+  add(productId: number, productPrice: number) {
     const accessToken = localStorage.getItem('accessToken') || '';
     const tokenPayload = this.helperService.decodeToken(accessToken);
     const userId = tokenPayload.userId;
@@ -43,10 +47,9 @@ export class ProductComponent implements OnInit {
     this.basket.productQuantity = this.productQuantity;
     this.basket.productPrice = productPrice;
 
-    this.basketService.addBasket(this.basket).subscribe(result=>{
-      alert(result.data.productName + " sepete eklendi");
-    })
-
+    this.basketService.addBasket(this.basket).subscribe((result) => {
+      alert(result.data.productName + ' sepete eklendi');
+      this.router.navigate(["/hesabim"]);
+    });
   }
-
 }
--- Frontend/ETradefrontend/src/app/services/basket.service.ts
+++ Frontend/ETradefrontend/src/app/services/basket.service.ts
@@ -13,4 +13,13 @@ export class BasketService {
   addBasket(basket:Basket){
     return this.httpClient.post<any>(this.apiUrl,basket);
   }
+
+  getByAppUserId(userId:string){
+    let newApiUrl = `${this.apiUrl}/GetByAppUserId/${userId}`;
+    return this.httpClient.get<any>(newApiUrl);
+  }
+
+  deleteBasket(id:number){
+    return this.httpClient.delete<any>(`${this.apiUrl}/${id}`);
+  }
 }

b02336de273acfd72eed91107038ab3ac0ef3c1a
Afevkaya <Afevkaya5151@gmail.com>
2022-06-09 16:11:52 +0300

    Coding the add method in the product component ts file.

--- Frontend/ETradefrontend/src/app/main-pages/product/product.component.ts
+++ Frontend/ETradefrontend/src/app/main-pages/product/product.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Basket } from 'src/app/models/basket';
 import { Category } from 'src/app/models/category';
 import { Product } from 'src/app/models/product';
+import { BasketService } from 'src/app/services/basket.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -13,7 +16,9 @@ export class ProductComponent implements OnInit {
 
   product!:Product;
   category!:Category;
-  constructor(public productService:ProductService,private route: ActivatedRoute) { }
+  basket!: Basket;
+  productQuantity:number = 1;
+  constructor(public productService:ProductService,private route: ActivatedRoute,private helperService:JwtHelperService,private basketService:BasketService) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params=>{
@@ -24,6 +29,24 @@ export class ProductComponent implements OnInit {
         this.category = result.data.category;
       });
     });
+  }
+
+  add(productId:number, productPrice:number){
+    debugger
+    const accessToken = localStorage.getItem('accessToken') || '';
+    const tokenPayload = this.helperService.decodeToken(accessToken);
+    const userId = tokenPayload.userId;
+
+    this.basket = new Basket();
+    this.basket.productId = productId;
+    this.basket.appUserId = userId;
+    this.basket.productQuantity = this.productQuantity;
+    this.basket.productPrice = productPrice;
+
+    this.basketService.addBasket(this.basket).subscribe(result=>{
+      alert(result.data.productName + " sepete eklendi");
+    })
+
   }
 
 }

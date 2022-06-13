d927ef53dca4b776e1f5b6b9b0f944bfb780c80d
Afevkaya <Afevkaya5151@gmail.com>
2022-06-13 11:05:01 +0300

    Coding of customerguard refreshToken' operations.

--- Frontend/ETradefrontend/src/app/services/customerguard.guard.ts
+++ Frontend/ETradefrontend/src/app/services/customerguard.guard.ts
@@ -1,37 +1,76 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { RefreshToken } from '../models/refresh-token';
 import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CustomerguardGuard implements CanActivate {
-  /**
-   *
-   */
-   constructor(public authService:AuthService, public router:Router,private helperService:JwtHelperService) { }
+  refreshToken!: RefreshToken;
+  constructor(
+    public authService: AuthService,
+    public router: Router,
+    private helperService: JwtHelperService
+  ) {}
 
-   canActivate(route: ActivatedRouteSnapshot) {
+  canActivate(route: ActivatedRouteSnapshot) {
+    const expectedRole = route.data.expectedRole;
+    const accessToken = localStorage.getItem('accessToken') || '';
+    const refreshToken = localStorage.getItem('refreshToken');
 
-     const expectedRole = route.data.expectedRole;
-     const accessToken = localStorage.getItem('accessToken') || '';
+    const tokenPayload = this.helperService.decodeToken(accessToken);
+    const refreshTokenExpiration = localStorage.getItem(
+      'refreshTokenExpiration'
+    );
 
-     const tokenPayload = this.helperService.decodeToken(accessToken);
 
-     if(!this.authService.isAuthenticated() || tokenPayload.roles !== expectedRole){
-       this.router.navigate(['auth/customerlogin']);
-       return false;
-     }
-     return true;
+    let now = new Date();
+    let resfreshDate;
 
-     // if(this.authService.isLoggedIn()){
-     //   return true;
-     // }
-     // else{
-     //   this.router.navigate(["auth/adminlogin"]);
-     //   return false;
-     // }
+    // Otantike değilse
+    if (!this.authService.isAuthenticated()) {
+      // RefreshToken kontrolü
+      if (refreshToken == null) {
+        this.router.navigate(['auth/customerlogin']);
+        return false;
+      }
+      // RefreshToken varsa tarih kontrolü
+      else {
+        resfreshDate = new Date(refreshTokenExpiration || now);
+        if (now.toISOString() > resfreshDate.toISOString()) {
+          this.router.navigate(['auth/customerlogin']);
+          return false;
+        }
+      }
 
-   }
+      this.refreshToken = new RefreshToken();
+      this.refreshToken.token = refreshToken || '';
+
+      // refreshtoken'a göre yeni token üretme
+      this.authService
+        .createTokenByRefreshtoken(this.refreshToken)
+        .subscribe((result) => {
+          console.log(result.data);
+          localStorage.setItem('accessToken', result.data.accessToken);
+          localStorage.setItem('refreshToken', result.data.refreshToken);
+          localStorage.setItem(
+            'accessTokenExpiration',
+            result.data.accessTokenExpiration
+          );
+          localStorage.setItem(
+            'refreshTokenExpiration',
+            result.data.refreshTokenExpiration
+          );
+        });
+    }
+    if(tokenPayload.roles != null){
+      if (tokenPayload.roles !== expectedRole) {
+        this.router.navigate(['auth/customerlogin']);
+        return false;
+      }
+    }
+    return true;
+  }
 }
